f5d80b3dc1054b96c0722a907e808b2fabedb3be
Soseh <sosehv@gmail.com>
2025-02-27 12:07:24 -0500

    Updates queryString for getAllProperties function

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -112,7 +112,7 @@ const getAllProperties = (options, limit = 10) => {
 
   let queryString = `SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id
+  LEFT JOIN property_reviews ON properties.id = property_id
   `;
 
   let whereClauses = [];
